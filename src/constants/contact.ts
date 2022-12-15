88a9e3e7779a22f571e6283be39e6763b4ee68ae
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2022-12-15 15:44:29 -0300

    alteração do numero

--- src/constants/contact.ts
+++ src/constants/contact.ts
@@ -1,7 +1,7 @@
 export const email = "joaogabrielmgtsu@gmail.com";
 
 export const number = "+55 47 9 9961-5822";
-export const whatsappLink = "https://wa.me/554799961582";
+export const whatsappLink = "https://wa.me/5547999615822";
 
 export const linkedin =
   "https://www.linkedin.com/in/jo%C3%A3o-gabriel-731948196/";
